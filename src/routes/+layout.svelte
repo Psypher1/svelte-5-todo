fbd8fbf2618e75e9cbd3d620bc4a1a7661fdbcf5
dante <jamesmidzi@gmail.com>
2024-04-20 22:18:42 +0200

    finally learning svelte 5

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {import('./$types').LayoutData} */
+
+	import "../app.css";
+</script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		padding: var(--size-10);
+		display: grid;
+		place-items: center;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+	// let todo = [] svelte 4
+	let todos = $state([
+		{ text: "learn svelte", done: false },
+		{ text: "learn turso", done: true },
+		{ text: "learn sveltekit", done: false }
+	]);
+</script>
